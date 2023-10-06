a68743fbf298e89c722b5dec02b221d3e7826596
semra-elcelik <semraelcelik@gmail.com>
2023-10-06 04:13:29 +0300

    useLocalStorage and UseGeceModu hooks

---
+++ frontend/hooks/useGeceModu.js
@@ -0,0 +1,10 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useGeceModu = (initialValue) => {
+  const [geceModu, setGeceModu] = useLocalStorage("geceModu", initialValue);
+
+  const changeValue = (value) => {
+    setGeceModu(value);
+  };
+  return [geceModu, changeValue];
+};
